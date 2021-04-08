480fb87b436addd759ea147d156dc86cc9fb00b5
w13b3 <w13b3>
2021-04-08 23:41:55 +0200

    update and add some better local try out code removed the thread version

--- capture_screen/capture_screen_multiprocess.py
+++ capture_screen/capture_screen_multiprocess.py
@@ -1,76 +1,119 @@
 #!/usr/bin/env python3
-import multiprocessing
-import cv2  # opencv-python
+# -*- coding: utf-8 -*-
+# capture_screen_multiprocess.py
+
+import logging
+from queue import Full, Empty
+from multiprocessing import Process, Queue, Array, set_start_method
+
 import mss
+import mss.tools
+from mss.screenshot import ScreenShot
+from mss import ScreenShotError
+
 import numpy as np
 
 
-class Capture(multiprocessing.Process):
-    """ Capture the screen
+class ScreenCapture(Process):
 
-    Little code snippet below showing how to use this class:
+    def __init__(self, queue: Queue, bbox: Array):
+        super(ScreenCapture, self).__init__(daemon=True)
+        self._queue = queue
+        self.bbox = bbox
+        self._running = True
 
-    import multiprocessing
-    import cv2  # opencv-python
-    import mss
-    import numpy as np
+    def run(self) -> None:
+        """ starts when ScreenCapture.start() is called """
+        logging.info(f"{self.name} start process")
+        self.__start_screen_capture()
 
-    ...
+    def stop(self) -> None:
+        """ stop the screen capture """
+        logging.info(f"{self.name} stop process")
+        self._running = not bool(self._running)
+        self.terminate()  # stop the process
+        self.join(timeout=5)  # join with the main
 
-    if __name__ == "__main__":
-        monitor = {'top': 100, 'left': 100, 'width': 200, 'height': 200}  # size and position of the captured screen.
-
-        cap = Capture(monitor=monitor, daemon=True)  # <- daemon process
-        cap.start()  # start the process
-
-        while True:  # the screen is being capture
-            if cv2.waitKey(1) & 0xFF == ord("q"):  # when 'q' is typed stop the loop
-                cv2.destroyAllWindows()  # close all the windows made
-                break
-            if not cap.queue.empty():  # if the queue is not empty
-                image = cap.queue.get_nowait()  # get a frame
-                cap.queue.task_done()  # tell the queue that the frame is received
-                cv2.imshow("screen capture", image)  # show the frame
-
-        [proc.terminate() for proc in cap.processes]  # terminate all multiprocesses
-    """
-    queue = multiprocessing.JoinableQueue
-    processes = set()
-
-    def __new__(cls, *args, **kwargs):
-        # create instance and paste it into processes
-        instance = super(__class__, cls).__new__(cls)
-        cls.processes.add(instance)  # add instances to the class
-        cls.queue = multiprocessing.JoinableQueue()  # every instance it's own queue
-        return instance
-
-    def __init__(self, monitor, *args, **kwargs):
-        super(Capture, self).__init__(*args, **kwargs)
-        assert isinstance(monitor, dict), "monitor needs to be a type(dict)"
-        assert {"top", "left", "height", "width"} <= set(monitor), "missing keys in monitor dict"
-        self.monitor = monitor  # e.g. {'top': 100, 'left': 100, 'width': 200, 'height': 200}
-
-    def run(self):
+    def __start_screen_capture(self) -> None:
+        """ loop that puts the screenshot made into the queue """
         with mss.mss() as sct:
-            while True:
-                img = np.array(sct.grab(self.monitor))  # capture the screen
-                self.queue.put_nowait(img)  # put the numpy.ndarray into the queue
-                self.queue.join()
+            logging.debug(f"{self.name} start screen capture: {self._running}")
+            while self._running:
+                try:
+                    area = (*self.bbox[:],)                 # PIL bbox tuple
+                    img_grab: ScreenShot = sct.grab(area)   # make a screenshot
+                    np_arr = np.asarray(img_grab)           # convert to np.ndarray
+                    self._queue.put(np_arr, timeout=0.01)   # put it in the queue
+                except ScreenShotError:
+                    msg = f"{self.name}, ScreenShotError bbox: {area}"
+                    logging.exception(msg)
+                    break
+                except KeyboardInterrupt:
+                    msg = f"{self.name}, KeyboardInterrupt"
+                    logging.exception(msg)
+                    break
+                except Full:  # when the queue is full, ignore it
+                    continue  # next loop
 
 
 if __name__ == "__main__":
-    monitor = {'top': 100, 'left': 100, 'width': 200, 'height': 200}  # size and position of the captured screen.
+    """ mirror the image of the Viewport onto the Porthole """
+    try:
+        # opencv-python
+        import cv2.cv2 as cv
+    except ImportError:
+        import cv2 as cv
 
-    cap = Capture(monitor=monitor, daemon=True)  # <- deamon process
-    cap.start()  # start the process
+    # create the Viewport window
+    viewport = 'Viewport'
+    cv.namedWindow(viewport, cv.WINDOW_GUI_NORMAL)
+    cv.setWindowProperty(viewport, cv.WND_PROP_AUTOSIZE, cv.WINDOW_NORMAL)
+    cv.setWindowProperty(viewport, cv.WND_PROP_ASPECT_RATIO, cv.WINDOW_FREERATIO)
 
-    while True:  # the screen is being capture
-        if cv2.waitKey(1) & 0xFF == ord("q"):  # when 'q' is typed stop the loop
-            cv2.destroyAllWindows()  # close all the windows made
+    set_start_method('spawn')  # windows default
+
+    img_queue = Queue(maxsize=3)  # always up to date with a buffer of 3 frames
+    # shared memory array with 4 places (left, upper, right, lower)
+    monitor = Array('i', (1, 1, 2, 2))
+
+    # start making screenshots
+    screen_capture = ScreenCapture(img_queue, monitor)
+    screen_capture.start()
+
+    # use mss to get the width and height of the monitor(s)
+    mon: dict = mss.mss().monitors[0]
+    mon_width = mon.get('width')
+    mon_height = mon.get('height')
+
+    while screen_capture.is_alive():
+        try:
+            # get the image from the queue that is put in by ScreenCapture
+            img = img_queue.get_nowait()
+            # show the image in the Porthole
+            cv.imshow('Porthole', img)
+
+            # get the location of the black square on the Viewport window
+            w_left, w_top, w_width, w_height = cv.getWindowImageRect(viewport)
+            # Convert PIL bbox style (left, upper, right, lower)
+            left, upper, right, lower = w_left, w_top, (w_left + w_width), (w_top + w_height)
+            # make sure the window never gets out of the screen
+            left = left if 0 <= left else 0
+            upper = upper if 0 <= upper else 0
+            right = right if right < mon_width else mon_width
+            lower = lower if lower < mon_height else mon_height
+            # set it to the monitor array so the ScreenCapture process can use it
+            monitor[:] = (left, upper, right, lower)
+
+            if not bool(cv.getWindowProperty(viewport, cv.WND_PROP_VISIBLE)):
+                break  # if the Viewport is closed
+            if cv.waitKey(1) & 0xFF == ord("q"):
+                break  # if Q is pressed
+        except Empty:
+            continue
+        except KeyboardInterrupt:
             break
-        if not cap.queue.empty():  # if the queue is not empty
-            image = cap.queue.get_nowait()  # get a frame
-            cap.queue.task_done()  # tell the queue that the frame is received
-            cv2.imshow("screen capture", image)  # show the frame
 
-    [proc.terminate() for proc in cap.processes]  # terminate all multiprocesses
+    # clean up
+    cv.destroyAllWindows()
+    screen_capture.stop()
+    screen_capture.join()
